7f81f4a760fe87136247cea9727fd374480e1a5f
s1300150 <s1300150@ilab2dc33.u-aizu.ac.jp>
2023-06-15 17:50:09 +0900

    Won/lost message added

--- dicegame.c
+++ dicegame.c
@@ -1,8 +1,15 @@
 #include<stdlib.h>
 #include<time.h>
 #include<stdio.h>
+#define MAX 20
 
 int main(){
+  char name[MAX];
+  printf("What is your name?\n");
+  printf("> ");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+  
   srand((unsigned int)time(NULL));
   printf("Rolling the dice...\n");
   
@@ -16,6 +23,13 @@ int main(){
   sum += y;
   
   printf("Total value: %d\n",sum);
+  
+  if(sum >= 7){
+    printf("You won!\n");
+  }
+  else{
+    printf("You lost!\n");
+  }
 
   return 0;
 }
